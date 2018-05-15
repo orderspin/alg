fa0251cb4c90377e883517b0ea1ffbaf9faf3ce4
orderspin <490522850@qq.com>
2018-05-15 20:43:24 +0900

    N

--- sort.cpp
+++
@@ -1,7 +0,0 @@
-Sort::Sort(Vector<int> (*t_func)(Vector<int>), int t_size): func(t_func), size(t_size) {
-
-};
-
-Sort::doit() {
-    this->sorted = this->func(original);
-};
--- sort.hpp
+++ sort.hpp
@@ -1,18 +1,79 @@
-#ifndef SORTING_H
-#define SORTING_H
+#ifndef SORT_H
+#define SORT_H
 
-#include <ctime>
-#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "tool.hpp"
 
-struct Sort {
-    Vector<int> original;
-    Vector<int> sorted;
+void exchange(std::vector<int> &input, int i, int j);
 
-    int size;
-    double timeCost;
-    Vector<int> (*func)(Vector<int>);
+void selection(std::vector<int> &to_sort);
+void insertion(std::vector<int> &to_sort);
+void shell(std::vector<int> &to_sort);
+void bubble(std::vector<int> &to_sort);
+void merge(std::vector<int> &to_sort);
 
-    void doit();
-};
+void exchange(std::vector<int> &input, int i, int j) {
+    if (i != j) {
+        int tmp = input[i];
+        input[i] = input[j];
+        input[j] = tmp;
+    }
+}
+
+void selection(std::vector<int> &to_sort) {
+    int N = to_sort.size();
+    for (int i = 0; i < N; i++) {
+        int min = i;
+        for (int j = i + 1; j < N; j++) {
+            if (to_sort[j] < to_sort[min]) {
+                min = j;
+            }
+        }
+        exchange(to_sort, min, i);
+    }
+}
+
+void insertion(std::vector<int> &to_sort) {
+    int N = to_sort.size();
+    for (int i = 1; i < N; i++) {
+        for (int j = i; j > 0 && to_sort[j] < to_sort[j - 1]; j--) {
+            exchange(to_sort, j, j - 1);
+        }
+    }
+}
+
+void shell(std::vector<int> &to_sort) {
+    int N = to_sort.size();
+    int h = 1;
+    while (h < N / 3) {
+        h = h * 3 + 1;
+    }
+    while (h > 0) {
+        for (int i = h; i < N; i++) {
+            for (int j = i; j >= h && to_sort[j] < to_sort[j - h]; j -= h) {
+                exchange(to_sort, j, j - h);
+            }
+            
+        }
+        h /= 3;
+    }
+}
+
+void bubble(std::vector<int> &to_sort) {
+    int N = to_sort.size();
+    for (int i = N; i > 0; i--) {
+        for (int j = 1; j < i; j++) {
+            if (to_sort[j] < to_sort[j - 1]) {
+                exchange(to_sort, j, j - 1);
+            }
+        }
+    }
+}
+
+void merge(std::vector<int> &to_sort) {
+
+}
 
 #endif
